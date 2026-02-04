5895008f2b2b0a5570e18ec8f9f1102690a72332
Denis Orlov <des2033@gmail.com>
2026-02-04 19:53:51 +0400

    2 version

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -1,7 +1,5 @@
 package ru.practicum.moviehub;
 
-import com.sun.net.httpserver.HttpExchange;
-import ru.practicum.moviehub.http.BaseHttpHandler;
 import ru.practicum.moviehub.http.MoviesServer;
 import ru.practicum.moviehub.store.MoviesStore;
 
